b113a93fba46a463e92059690229fc3e864ad835
Roshni Pradhan <roshnipradhan2006@gmail.com>
2026-03-31 14:41:58 +0530

    Implement expense prediction using Linear Regression

--- model/expense_predictor.py
+++ model/expense_predictor.py
@@ -1,1 +1,14 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
+data = pd.read_csv("expense_data.csv")
+
+X = data[['Income','Day','PreviousExpense']]
+y = data['Expense']
+
+model = LinearRegression()
+model.fit(X,y)
+
+prediction = model.predict([[25000,1,400]])
+
+print("Predicted Expense:", prediction[0])
